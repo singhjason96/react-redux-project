de58556f6b1618fc3c60690586877b474b17ac68
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-10-25 12:05:49 -0400

    added sign in form component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
