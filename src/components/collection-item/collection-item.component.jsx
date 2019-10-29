da0fccdb1ee0992b9a02c4b6de9c25d7455ac22d
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-10-29 14:17:44 -0400

    updated cart utiities to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import {connect} from 'react-redux';
+import {addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="ui card">
+const CollectionItem = ({item, addItem}) => {
+    const { name, price, imageUrl } = item;
+    return(
+    <div className="card">
         
             <img src={`${imageUrl}`} className="image" />
         
-        <div className="description">
+        <div className="card-body">
                 <span className="name">{name}</span>
-                <span className="price">${price}</span>
+                <span className="price">  ${price}</span>
         </div>
+        <CustomButton inverted onClick={() => addItem(item)}>Add to Cart</CustomButton>
     </div>
-)
 
-export default CollectionItem;
+)}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,8 +7,8 @@ const CollectionPreview = ({title, items}) => (
         <h1 className="title">{title}</h1>
         <div className="preview">
             {
-                items.map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
+                items.map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
 
